e8b088190a50fc713349b68a6740e314ee21f87e
jatin kumar <jatinkumarr096@gmail.com>
2024-03-05 21:14:50 -0500

    updated code

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/******************************************************************************
+ *	ITE5315 – Assignment 2
+ *	I declare that this assignment is my own work in accordance with Humber Academic Policy.   *  No part of this assignment has been copied manually or electronically from any other source *  (including web sites) or distributed to other students.
+ *
+ *	Name: __Jatin Kumar__ Student ID: __N01552458__ Date: _05/02/2024__
+ *
+ *
+ ******************************************************************************/
+
 var express = require("express");
 var path = require("path");
 var app = express();
